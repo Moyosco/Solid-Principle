0cb074b6b8f3c5b9ffb8a45f9d5fce1e1dfa645a
Moyosore <110602661+Moyosco@users.noreply.github.com>
2022-08-12 08:44:26 +0100

    Add files via upload

---
+++ Solid Principle/Solid Principle/Paymentdetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principle
+{
+    internal class Paymentvalidation : classdetails  //instantiating class
+    {
+        private int Numberofstudent;
+        public int Numberofstudents { get; set; }
+        public int Numberofcourses { get; set; }
+        public string Paymentdetails { get; set; }   
+        public string Score()
+        {     
+            try
+             {
+                if (Numberofstudent >= Numberofcourses)
+                {
+                    return "You are registered with the school";
+                }
+                else if (Numberofstudents > Numberofcourses)
+                {
+                    return "You are not registered with the school";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception Bimbo)
+            {
+
+                return "error";
+            }
+        }
+        
+    }
+}
---
+++ Solid Principle/Solid Principle/Userdetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principle
+{
+    internal class Studentdetails
+    {
+
+        public string Name { get; set; }    
+        public string Email { get; set; }   
+        public int Matricnumber { get; set; }
+        public bool Isstudent { get; set; }
+        public bool studentvalidator()
+        {
+            try
+            {
+                if (Isstudent)
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+                  
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+       
+
+    }
+}
